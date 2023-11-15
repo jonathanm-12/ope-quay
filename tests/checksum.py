b0e64c231d389e333cca64e45c39c22b067923b2
jonathan <jmikalov@bu.edu>
2023-11-15 02:51:38 +0000

    modified checksum

--- tests/checksum.py
+++ tests/checksum.py
@@ -12,13 +12,15 @@ OPE_BOOK_IMAGE = data['ope']['book_user'] + '/' + data['ope']['book']
 
 OPE_BETA_TAG = "beta-ope"
 
-ARGS = "find / -not \\( -path /proc -prune \\) -not \\( -path /sys -prune \\) -type f -exec stat -c '%n %a' {} + | LC_ALL=C sorat | sha256sum | cut -c 1-64"
+ARGS = "find / -not \( -path /proc -prune \) -not \( -path /sys -prune \) -type f -exec stat -c '%n %a' {} + | LC_ALL=C sort | sha256sum | cut -c 1-64"
 
 DARGS = "-u 0"
 
 #CHECKSUM = subprocess.check_output(command, shell=True, text=True, stderr=subprocess.DEVNULL)
 
 RUN_DOCKER = "docker run -i --rm %s %s %s %s %s" % (DARGS, OPE_BOOK_REG, OPE_BOOK_IMAGE, OPE_BETA_TAG, ARGS)
+
+print(RUN_DOCKER)
 
 CHECKSUM = subprocess.check_output(RUN_DOCKER, shell=True, stderr=subprocess.DEVNULL).decode('utf-8')
 
